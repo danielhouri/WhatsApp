c6cabe92ab1279996f09d4f09749bcf6136658a6
dorhuri123 <dor.huri1@live.biu.ac.il>
2022-04-09 04:42:06 +0300

    11

---
+++ src/Chat/Components/Messages/FilePopUp.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import "./FilePopUp.css"
+
+function FilePopUp(props) {
+    return (
+        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="staticBackdropLabel">Modal title</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <button className="btn btn-outline-secondary" type="button">Upload</button>
+                            </div>
+                            <div className="custom-file">
+                                <input type="file" className="custom-file-input" id="inputGroupFile03"/>
+                                <label className="custom-file-label" htmlFor="inputGroupFile03">Choose file</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary">Understood</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FilePopUp
--- src/Chat/Components/Messages/Messages.js
+++ src/Chat/Components/Messages/Messages.js
@@ -5,11 +5,13 @@ import { getImageByUsername, getNicknameByUsername, getMessageByUsername, addNew
 import "../ChatLog";
 import MessagesBody from './MessagesBody';
 import VoicePopUp from '../VoiceRecorder/VoicePopUp';
+import FilePopUp from './FilePopUp';
 
 function Messages(props) {
   const [voiceMessage, setVoiceMessage] = useState(false);
+  const [fileWindow, setFileWindow] = useState(false);
   const chatBox = useRef(null);
-
+  const [toolBar, setToolBar] = useState(true)
   const handleSendMessage = () => {
     const d = new Date();
     let time = d.getHours() + ":" + d.getMinutes();
@@ -17,7 +19,9 @@ function Messages(props) {
     chatBox.current.value = '';
     props.setRefresh(props.refresh + 1);
   };
-
+  const handleToolBar = () => {
+    setToolBar(!toolBar)
+  }
   const handleEnterMessage = (e) => {
     if (e.key === 'Enter') {
       e.preventDefault();
@@ -32,7 +36,12 @@ function Messages(props) {
   const handleCloseVoice = () => {
     setVoiceMessage(false)
   }
-  
+  const handleOpenChoosFile = () => {
+    setFileWindow(true)
+  }
+  const handleCloseChoosFile = () => {
+    setFileWindow(false)
+  }
   return (
     <div className="messages">
       <div className='messages_header'>
@@ -41,16 +50,23 @@ function Messages(props) {
           <h3>{getNicknameByUsername(props.username)}</h3>
         </div>
       </div>
-      <MessagesBody listChat={getMessageByUsername(props.username)} />
+      <MessagesBody listChat={getMessageByUsername(props.username)}/>
+      
+      <div hidden={toolBar} className='toolbar'>
+          <i className="bi bi-image" onClick={handleOpenChoosFile} data-bs-toggle="modal" data-bs-target="#staticBackdrop"></i>
+          <i className="bi bi-camera-reels"></i>
+          <i className="bi bi-geo-alt"></i> 
+      </div>
       <div className='messages_footer'>
         <i className="bi bi-mic-fill" onClick={handleOpenVoice}> </i>
-        <i className="bi bi-paperclip"></i>
+        <i className="bi bi-paperclip" onClick={handleToolBar}></i>
         <form>
           <input ref={chatBox} placeholder='Type a Message' onKeyPress={handleEnterMessage} />
         </form>
         <i onClick={handleSendMessage} type="submit" className="bi bi-send-fill"> </i>
       </div>
-      <VoicePopUp show={voiceMessage} handleClose={handleCloseVoice} username={props.username} refresh={props.refresh} setRefresh={props.setRefresh}/>
+      <VoicePopUp show={voiceMessage} handleClose={handleCloseVoice} username={props.username} refresh={props.refresh} setRefresh={props.setRefresh} />
+      <FilePopUp show={fileWindow} handleClose={handleCloseChoosFile} username={props.username}/>
     </div>
   )
 }
