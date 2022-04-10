e456bc77702b0de46f6341f255b7983b023d45cc
dorhuri123 <dor.huri1@live.biu.ac.il>
2022-04-10 12:48:38 +0300

    aaa

---
+++ src/Chat/Components/SideBarChat/SideBarChat.js
@@ -0,0 +1,29 @@
+import { Avatar } from '@mui/material';
+import React from 'react';
+import './SideBarChat.css';
+
+function SideBarChat(props) {
+    const handleClicked = function () {
+        props.setBackgroundShow(props.username);
+    }
+    return (
+        <div className='sidebarchat container' onClick={handleClicked}>
+            <div className='row'>
+                <div className='col col-2 sidebarchat_avatar'>
+                    <Avatar src={props.src} />
+                </div>
+                <div className='col col-8'>
+                    <div className='sidebarchat_info'>
+                        <h2>{props.nickname}</h2>
+                        <p>{props.message_content}</p>
+                    </div>
+                </div>
+                <div className='col col-2'>
+                    <div className="sidebarchat_time"> {props.message_time} </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SideBarChat;
